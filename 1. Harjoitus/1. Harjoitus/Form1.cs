969f32a1a524b4721ba3272e09ad1159514533f4
Linara Vakhitova <swanschimmel@gmail.com>
2026-02-13 19:54:00 +0200

    1. Harjoitus

---
+++ 1. Harjoitus/1. Harjoitus/Form1.cs
@@ -0,0 +1,15 @@
+namespace _1._Harjoitus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void VaihdaBT_Click(object sender, EventArgs e)
+        {
+            OtsikkoLB.Text = "Heippa maailma!"; 
+        }
+    }
+}
