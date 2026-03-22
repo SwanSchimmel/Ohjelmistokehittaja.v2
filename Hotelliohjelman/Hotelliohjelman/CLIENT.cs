f9d9cc58d49745ac6d33086f35125e3f4f77210a
Linara Vakhitova <swanschimmel@gmail.com>
2026-03-22 23:14:27 +0200

    finish

--- Hotelliohjelman/Hotelliohjelman/CLIENT.cs
+++ Hotelliohjelman/Hotelliohjelman/CLIENT.cs
@@ -1,11 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 
 namespace Hotelliohjelman
